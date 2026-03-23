53718b0ce7eb9fd037a248ad78e2465bd938e03e
Pearl sethi <155882640+pearl24680@users.noreply.github.com>
2026-03-23 11:20:25 +0530

    Fix pip install command syntax

--- notes_app_manager.py
+++ notes_app_manager.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1-p2pa50dzyPVYH3HiGkq5ysHoqOlxoYu
 """
 
-!pip install streamlit pandas openpyxl
+pip install streamlit pandas openpyxl
 
 import streamlit as st
 import pandas as pd
